cb28ddfc67e8ff8b2bc0944b6d897209edee0e09
Israimo <kalungeraymond93@gmail.com>
2025-06-20 00:36:37 +0300

    Update initFirebase.ts

--- client/lib/initFirebase.ts
+++ client/lib/initFirebase.ts
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
-
+import { getAnalytics } from "firebase/analytics";
 const firebaseConfig = {
-  apiKey: "AIzaSyCFeYMHAYAkwG4RmwMTezPPpD1juj9g2NQ",
-  authDomain: "maven-engine.firebaseapp.com",
-  projectId: "maven-engine",
-  storageBucket: "maven-engine.appspot.com",
-  messagingSenderId: "960950688648",
-  appId: "1:960950688648:web:f941494b053ca09415383d",
-  measurementId: "G-7L0B5JVKEL",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export default app;
+const analytics = getAnalytics(app);
+
+export { app, analytics };
